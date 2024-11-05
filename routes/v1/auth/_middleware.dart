bdd3d56440ff3bd12b7de50b49e033a41425fac3
munyaaa <jessicajimantoro@gmail.com>
2024-11-05 20:03:41 +0700

    feat: add register on route

--- routes/v1/auth/_middleware.dart
+++ routes/v1/auth/_middleware.dart
@@ -1,7 +1,29 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:petto_api/middlewares/db_middleware.dart';
+import 'package:postgres/postgres.dart';
+import 'package:repositories/repositories.dart';
+import 'package:services/services.dart';
 
 Handler middleware(Handler handler) {
-  return handler.use(
-    requestLogger(),
-  );
+  return handler
+      .use(
+        provider<AuthService>(
+          (context) => AuthServiceImpl(
+            authRepository: context.read<AuthRepository>(),
+          ),
+        ),
+      )
+      .use(
+        provider<AuthRepository>(
+          (context) => AuthRepositoryImpl(
+            db: context.read<Future<Connection>>(),
+          ),
+        ),
+      )
+      .use(
+        dbProvider(),
+      )
+      .use(
+        requestLogger(),
+      );
 }
--- routes/v1/auth/register.dart
+++ routes/v1/auth/register.dart
@@ -1,6 +1,39 @@
-import 'package:dart_frog/dart_frog.dart';
+import 'dart:async';
+import 'dart:io';
 
-Response onRequest(RequestContext context) {
-  // TODO: implement route handler
-  return Response(body: 'This is a new route!');
+import 'package:dart_frog/dart_frog.dart';
+import 'package:domain/domain.dart';
+import 'package:services/services.dart';
+
+FutureOr<Response> onRequest(RequestContext context) {
+  return switch (context.request.method) {
+    HttpMethod.post => _onPost(context),
+    _ => Future.value(
+        Response(statusCode: HttpStatus.methodNotAllowed),
+      ),
+  };
+}
+
+Future<Response> _onPost(RequestContext context) async {
+  final authService = context.read<AuthService>();
+
+  try {
+    final body = await context.request.json() as Map<String, dynamic>?;
+    final request = RegisterRequest.fromJson(body ?? {});
+
+    final userId = await authService.register(request);
+
+    return Response.json(
+      body: {
+        'data': {
+          'id': userId,
+        },
+      },
+    );
+  } catch (e) {
+    return Response.json(
+      statusCode: 500,
+      body: e.toString(),
+    );
+  }
 }
