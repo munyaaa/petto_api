f4400f0781c0c2bbdad662aceacb8f7e4ba2355b
munyaaa <jessicajimantoro@gmail.com>
2024-11-05 21:16:18 +0700

    feat: add login on service

--- packages/services/lib/src/services/auth_service.dart
+++ packages/services/lib/src/services/auth_service.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:bcrypt/bcrypt.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:domain/domain.dart';
 import 'package:repositories/repositories.dart';
 
 abstract class AuthService {
   Future<int> register(RegisterRequest request);
+  Future<String> login(LoginRequest request);
 }
 
 class AuthServiceImpl implements AuthService {
@@ -35,5 +39,51 @@ class AuthServiceImpl implements AuthService {
     }
 
     return userId;
+  }
+
+  @override
+  Future<String> login(LoginRequest request) async {
+    final email = request.email ?? '';
+    if (email.isEmpty) {
+      throw Exception('Email cannot be empty!');
+    }
+
+    final password = request.password ?? '';
+    if (password.isEmpty) {
+      throw Exception('Password cannot be empty!');
+    }
+
+    final user = await authRepository.getUser(email: email);
+
+    if (user == null) {
+      throw Exception('User not found!');
+    }
+
+    if (!BCrypt.checkpw(request.password ?? '', user.hashedPassword)) {
+      throw Exception('Wrong password!');
+    }
+
+    final token = _generateToken(user.id);
+    if (token == null) {
+      throw Exception('Failed to generate token!');
+    }
+
+    return token;
+  }
+
+  String? _generateToken(int id) {
+    final jwt = JWT({
+      'sub': id.toString(),
+    });
+
+    final tokenSecret = Platform.environment['TOKEN_SECRET'];
+
+    if (tokenSecret == null) {
+      return null;
+    }
+
+    return jwt.sign(
+      SecretKey(tokenSecret),
+    );
   }
 }
