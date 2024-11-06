0fbdcc77c5671627f7503a327b8047f31295f838
munyaaa <jessicajimantoro@gmail.com>
2024-11-06 18:07:43 +0700

    feat: add bearer authentication middleware

---
+++ lib/middlewares/bearer_authentication_middleware.dart
@@ -0,0 +1,26 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_auth/dart_frog_auth.dart';
+import 'package:services/services.dart';
+
+Middleware bearerAuthenticationMiddleware() {
+  return (handler) {
+    return handler.use(
+      bearerAuthentication<int>(
+        authenticator: (context, token) async {
+          try {
+            final tokenService = context.read<TokenService>();
+            final userId = tokenService.verifyToken(token);
+
+            if (userId == null) {
+              return null;
+            }
+
+            return userId;
+          } catch (e) {
+            return null;
+          }
+        },
+      ),
+    );
+  };
+}
