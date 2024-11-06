a345183a9fb7f98416c797b6651b5736ada46ba0
munyaaa <jessicajimantoro@gmail.com>
2024-11-06 20:43:30 +0700

    feat: add get all pets at route

--- routes/v1/pets/index.dart
+++ routes/v1/pets/index.dart
@@ -8,6 +8,7 @@ import 'package:services/services.dart';
 
 FutureOr<Response> onRequest(RequestContext context) {
   return switch (context.request.method) {
+    HttpMethod.get => _onGet(context),
     HttpMethod.post => _onPost(context),
     _ => Future.value(
         Response(statusCode: HttpStatus.methodNotAllowed),
@@ -39,6 +40,49 @@ Future<Response> _onPost(RequestContext context) async {
             userId: userId,
             request: CreateUpdatePetRequest.fromJson(body ?? {}),
           ),
+        },
+      },
+    );
+  } catch (e) {
+    return Response.json(
+      statusCode: 500,
+      body: e.toString(),
+    );
+  } finally {
+    if (db.isOpen) {
+      await db.close();
+    }
+  }
+}
+
+Future<Response> _onGet(RequestContext context) async {
+  final tokenService = context.read<TokenService>();
+  final petService = context.read<PetService>();
+  final db = await context.read<Future<Connection>>();
+
+  try {
+    final token = context.request.headers['Authorization']
+        ?.replaceFirst('Bearer', '')
+        .trim();
+
+    final userId = tokenService.getUserIdByToken(token ?? '');
+
+    if (userId == null) {
+      throw Exception('Invalid user id');
+    }
+
+    final pets = await petService.getAllPets(
+      userId: userId,
+    );
+
+    return Response.json(
+      body: {
+        'data': {
+          'pets': pets
+              .map(
+                (e) => e.toJson(),
+              )
+              .toList(),
         },
       },
     );
